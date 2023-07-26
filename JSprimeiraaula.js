6e45b57a9951767b8206189401ef3387b500cc0e
abreur3beca <123771701+abreur3beca@users.noreply.github.com>
2023-07-26 14:16:09 -0300

    Add files via upload

---
+++ JSprimeiraaula.js
@@ -0,0 +1,89 @@
+// Meu nome é:
+console.log("Rebeca")
+// Atividade 1
+
+
+// Eu programei nenhuma semana nas férias
+console.log("0 semanas")
+// Atividade 2
+
+
+/*
+o // é para comentário de apenas uma linha
+enquanto o /* me permiti inserir mais de uma linha de comentário
+*/
+// Atividade 3
+
+
+// Variavel String
+let socorro = "Quero dormir";
+console.log(socorro)
+
+//Variavel Number
+let num = 18
+console.log(num)
+
+//Variavel Boolean
+let estouestudando = true
+console.log(estouestudando)
+
+//Variavel Objects
+const Dados = {
+    nome: 'Rebeca',
+    idade: '16'
+}
+console.log(Dados)
+
+//Variavel null
+let quantaspessoas = null
+console.log(quantaspessoas)
+//Atividade 4
+
+
+let idade = 16
+console.log(idade + 3.5)
+//Atividade 5
+
+
+let today = new Date();
+let year = today.getFullYear();
+console.log(year-1969)
+//Atividade 6
+
+console.log(65/240)
+//Atividade 7
+
+console.log(0.2708*100)
+//Atividade 8
+
+console.log("Hello" + "World")
+//Atividade 9
+
+console.log("Hello " + "World")
+//Atividade 10
+
+let caracteres = "Teaching the world how to code";
+console.log (caracteres.length);
+//Atividade 11
+
+let maisculas = 'Kaian Novais';
+console.log (maisculas.toUpperCase());
+//Atividade 12
+
+let espaco = ' Professor Kaian Novais ';
+console.log (espaco.trim());
+//Atividade 13
+
+let aleatoria = Math.random();
+console.log(aleatoria * 100);
+//Atividade 14
+
+let arredondar = Math.ceil(43.8);
+console.log (arredondar);
+//Atividade 15
+
+let inteiro = Number.isInteger(2017);
+console.log (inteiro);
+//Atividade 16
+
+
